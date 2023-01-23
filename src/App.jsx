7941c1dc6e96d0d7e6d0cd981f2bcdb9c7042168
ivan40kalpazan40 <46090032+ivan40kalpazan40@users.noreply.github.com>
2023-01-23 16:04:52 +0200

    create and style the outermost element

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import reactLogo from './assets/react.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div></div>;
+  return (
+    <div id="drum-machine">
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
