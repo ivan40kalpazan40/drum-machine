b00a2ece9f587bacddf18943b08dd31e02567527
ivan40kalpazan40 <46090032+ivan40kalpazan40@users.noreply.github.com>
2023-01-24 00:51:04 +0200

    play audio on button click with useRef hook

--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -1,10 +1,17 @@
+import { useRef } from 'react';
 const DrumPad = ({ pad, index }) => {
+  const audioFile = useRef(null);
   return (
-    <button className="drum-pad" id={index}>
+    <button
+      className="drum-pad"
+      id={index}
+      onClick={() => audioFile.current.play()}
+    >
       <audio
         className="clip"
         id={pad}
         src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+        ref={audioFile}
       ></audio>
       {pad}
     </button>
