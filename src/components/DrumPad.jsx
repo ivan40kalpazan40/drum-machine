d5ed4e18a2f78d485a68cb322661a1cfca442509
ivan40kalpazan40 <46090032+ivan40kalpazan40@users.noreply.github.com>
2023-01-23 23:43:10 +0200

    1 binaries uploaded

--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -1,6 +1,11 @@
 const DrumPad = ({ pad, index }) => {
   return (
     <button className="drum-pad" id={index}>
+      <audio
+        className="clip"
+        id={pad}
+        src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+      ></audio>
       {pad}
     </button>
   );
