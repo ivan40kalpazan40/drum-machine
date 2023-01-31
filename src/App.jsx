319d528c2b140d863ce04e3b81fe1f89f3a2661b
ivan40kalpazan40 <46090032+ivan40kalpazan40@users.noreply.github.com>
2023-01-31 14:13:31 +0200

    initial state setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 import KeyPad from './components/Keypad';
 import ControlPad from './components/ControlPad';
 
 function App() {
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      console.log(e.keyCode);
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
   return (
     <div id="drum-machine">
       <KeyPad />
--- src/slices/padSlice.js
+++ src/slices/padSlice.js
@@ -2,9 +2,54 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const padSlice = createSlice({
   name: 'pads',
-  initialState: {},
+  initialState: {
+    q: {
+      link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+      state: '',
+      code: 81,
+    },
+    w: {
+      link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+      state: '',
+      code: 87,
+    },
+    e: {
+      link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+      state: '',
+      code: 69,
+    },
+    a: {
+      link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+      state: '',
+      code: 65,
+    },
+    s: {
+      link: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+      state: '',
+      code: 83,
+    },
+    d: {
+      link: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+      state: '',
+      code: 68,
+    },
+    z: {
+      link: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+      status: '',
+      code: 90,
+    },
+    x: {
+      link: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+      status: '',
+      code: 88,
+    },
+    c: {
+      link: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+      state: '',
+      code: 67,
+    },
+  },
   reducers: {},
 });
-
 
 export default padSlice.reducer;
